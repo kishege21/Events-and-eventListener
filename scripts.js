e34cfb3669c038d4fc326225b1772d49555a158b
kishege21 <kishege21@gmail.com>
2020-06-06 21:03:01 +0200

    final

--- scripts.js
+++ scripts.js
@@ -1,4 +1,11 @@
-$("button").click(() => {
-    console.log("Yeah, you clicked me")
+$("#button1").click(() => {
+    alert("Yeah, you clicked me")
 });
 
+$("#button2").click(() => {
+    $("#button1").text("changed text")
+});
+
+$("#button3").click(() => {
+    $("button").css("background-color", "yellow")
+});
